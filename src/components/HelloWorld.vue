a821e140c7c38335a94b2e5d3e826fe975128edf
Gevorg Melkumyan <gevorg.melkumyan@polymorphic.io>
2023-07-17 11:20:05 +0400

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="align-center text-center fill-height">
+      <h1>Welcome to Tournament Generator!</h1>
+      <v-btn theme="dark" class="mt-3">Start a Tournament</v-btn>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+import axios from "axios";
+
+let loading = false
+
+function startTournament() {
+  axios.post()
+}
+</script>
