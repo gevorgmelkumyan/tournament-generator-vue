5c2f657d96760748ae12a450c8292c094335d8bd
Gevorg Melkumyan <gevorg.melkumyan@polymorphic.io>
2023-07-17 11:50:48 +0400

    Installed and configured axios, env

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,17 +2,29 @@
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
       <h1>Welcome to Tournament Generator!</h1>
-      <v-btn theme="dark" class="mt-3">Start a Tournament</v-btn>
+      <v-btn theme="dark" class="mt-3" @click="startTournament" :loading="loading">Start a Tournament</v-btn>
     </v-responsive>
   </v-container>
 </template>
 
-<script setup>
-import axios from "axios";
+<script>
+import axios from "axios"
 
-let loading = false
-
-function startTournament() {
-  axios.post()
+export default {
+  data() {
+    return {
+      loading: false
+    }
+  },
+  methods: {
+    startTournament() {
+      this.loading = true
+      axios.post(import.meta.env.VITE_API_URL + '/create_tournament')
+        .then(response => {
+          this.loading = false
+          console.log(response)
+        })
+    }
+  }
 }
 </script>
