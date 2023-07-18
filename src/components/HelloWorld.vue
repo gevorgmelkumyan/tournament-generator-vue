695b19e93f0e2c08de1c573f6fcc26c086acb8ea
Gevorg Melkumyan <gevorg.melkumyan@polymorphic.io>
2023-07-18 13:34:57 +0400

    Wrote a method to request the API to run division games

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,37 @@
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
       <h1>Welcome to Tournament Generator!</h1>
-      <v-btn theme="dark" class="mt-3" @click="startTournament" :loading="loading">Start a Tournament</v-btn>
+      <v-btn theme="dark" class="mt-3" @click="startTournament" :loading="loading">Create a Tournament</v-btn>
+
+      <div v-if="divisionA && divisionB">
+        <v-container>
+          <v-row>
+            <v-col>
+              <h1>Division A</h1>
+              <v-list>
+                <v-list-item
+                  v-for="team in divisionA"
+                  :key="team.name"
+                  :title="team.name"
+                ></v-list-item>
+              </v-list>
+            </v-col>
+
+            <v-col>
+              <h1>Division B</h1>
+              <v-list>
+                <v-list-item
+                  v-for="team in divisionB"
+                  :key="team.name"
+                  :title="team.name"
+                ></v-list-item>
+              </v-list>
+            </v-col>
+          </v-row>
+        </v-container>
+
+        <v-btn theme="dark" class="mt-3" @click="runDivisionGames" :loading="loading">Run Division Games</v-btn>
+      </div>
     </v-responsive>
   </v-container>
 </template>
@@ -13,7 +43,10 @@ import axios from "axios"
 export default {
   data() {
     return {
-      loading: false
+      loading: false,
+      divisionA: null,
+      divisionB: null,
+      tournament: null
     }
   },
   methods: {
@@ -22,7 +55,16 @@ export default {
       axios.post(import.meta.env.VITE_API_URL + '/create_tournament')
         .then(response => {
           this.loading = false
-          console.log(response)
+          this.divisionA = response.data.divisionA
+          this.divisionB = response.data.divisionB
+          this.tournament = response.data.tournament
+        })
+    },
+    runDivisionGames() {
+      this.loading = true
+      axios.post(import.meta.env.VITE_API_URL + `/run_division_games/${this.tournament.id}`)
+        .then(response => {
+          this.loading = false
         })
     }
   }
